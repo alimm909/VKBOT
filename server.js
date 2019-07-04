ca825a18f4584b9e61aebb9a4d289050c46b5114
Glitch (tartan-rain) <none>
2019-07-04 10:07:22 +0000

    🏅✍️ Checkpoint ./server.js:1934803/4

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const Discord = require('discord.js');
+-ğ
+üconst Discord = require('discord.js');
 const client = new Discord.Client();
 const ayarlar = require('./ayarlar.json');
 const chalk = require('chalk');
