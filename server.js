5ab196f078d9a82ab99ab3fb1fc0e09f71db2900
Glitch (tartan-rain) <none>
2020-02-13 20:13:31 +0000

    👀💒 Checkpoint ./server.js:4980160/24

--- server.js
+++ server.js
@@ -117,12 +117,6 @@ client.unload = command => {
   });
 };
 
-
-
-
-
-//dene
-
 client.elevation = message => {
   if(!message.guild) {
 	return; }
@@ -130,7 +124,7 @@ client.elevation = message => {
   if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
   if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
   if (message.author.id === ayarlar.sahip) permlvl = 4;
-  if (message.author.id === ayarlar.zpeed) permlvl = 4;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
 
   return permlvl;
 };
