5b2aea2d24f3cf19384d7d64e4d0fb893aeb0a70
Glitch (tartan-rain) <none>
2020-07-04 16:16:25 +0000

    🍞✂️ Checkpoint ./komutlar/seç2.js:18104349/2267

---
+++ komutlar/seç2.js
@@ -0,0 +1,48 @@
+const Discord = module.require('discord.js');
+const ayarlar = require('../ayarlar.json')
+const prefix = ayarlar.prefix
+const client = new Discord.Client();
+module.exports.run = async (client, message, args) => {
+  
+if(!message.member.roles.has("728928753215995914")) return message.channel.send('<a:hyr:728937091068854282> ・ Sadece **Yetkili** Bu Komudu Kullanabilir !')
+  
+let kanal = message.guild.channels.get(args[0]);
+       
+if(!kanal) {
+message.channel.send("<a:hyr:728937091068854282> ・ Rolleri Seçmek İçin Kanal İdsini Giriniz. Doğru kullanım **!seç3 <Sesli Kanal İd>**")
+} 
+if(kanal) {
+const embed = new Discord.RichEmbed()
+.setAuthor(message.guild.name, message.guild.iconURL)
+.setColor('RANDOM')
+.addField(
+            `Roller`
+,`1. Kurt: ` + kanal.members.random().user)
+          .addField(
+            `▬▬▬▬▬▬`
+,`2. Kurt: ` + kanal.members.random().user)
+          .addField(
+            `▬▬▬▬▬▬`
+,`Doktor: ` + kanal.members.random().user)
+          .addField(
+            `▬▬▬▬▬▬`
+,`Medyum: ` + kanal.members.random().user)
+          .addField(
+            `▬▬▬▬▬▬`
+,`Aura Gözcüsü: ` + kanal.members.random().user)
+          .addField(
+            `▬▬▬▬▬▬`
+,`Soytarı: ` + kanal.members.random().user)
+.setTimestamp()
+message.channel.send(embed)
+}
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['seç-2','seç2'],
+  permLevel: 0
+};
+exports.help = {
+  name: 'seç2'
+};
